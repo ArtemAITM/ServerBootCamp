c3d72cc5c6f31fc7a9ee532ea51529c7537e7a7c
ArtemAITM <antropovam2008@gmail.com>
2024-04-20 19:39:56 +0700

    аы

--- app/src/main/java/com/example/server_bootcamp/PlayType1.java
+++ app/src/main/java/com/example/server_bootcamp/PlayType1.java
@@ -7,7 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
+//вафы
 public class PlayType1 extends AppCompatActivity {
 
     @Override
