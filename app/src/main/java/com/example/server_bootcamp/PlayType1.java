b651c9b6df06fba4128ec5a8a77fa2e648612313
ArtemAITM <antropovam2008@gmail.com>
2024-04-20 20:10:34 +0700

    Mode 1 local connect succesfully

--- app/src/main/java/com/example/server_bootcamp/PlayType1.java
+++ app/src/main/java/com/example/server_bootcamp/PlayType1.java
@@ -13,7 +13,7 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PlayType1 extends AppCompatActivity {
-
+//1524
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
